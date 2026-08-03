17a564565cd43f689f102470168c4025269f6279
Srinath Vasudevan <srinath@opsera.io>
2026-08-04 00:20:30 +0530

    Add modules/terraform-aws-security-group/outputs.tf via CloudForge (inline module from Git)

---
+++ modules/terraform-aws-security-group/outputs.tf
@@ -0,0 +1,19 @@
+output "security_group_id" {
+  description = "ID of the security group."
+  value       = aws_security_group.this.id
+}
+
+output "security_group_arn" {
+  description = "ARN of the security group."
+  value       = aws_security_group.this.arn
+}
+
+output "security_group_name" {
+  description = "Name of the security group."
+  value       = aws_security_group.this.name
+}
+
+output "security_group_owner_id" {
+  description = "Owner ID of the security group."
+  value       = aws_security_group.this.owner_id
+}
