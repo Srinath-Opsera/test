f2e332494c057870e53216bd734ddf395a720c33
Srinath Vasudevan <srinath@opsera.io>
2026-08-05 02:28:33 +0530

    Update outputs.tf via CloudForge

--- outputs.tf
+++ outputs.tf
@@ -1,49 +1,29 @@
-output "log_group_names" {
-  description = "Map of log group keys to their names"
-  value       = module.cloudwatch_log_group.log_group_names
+output "secret_id" {
+  description = "The ID of the secret (same as the ARN)."
+  value       = module.secrets_manager_secret.secret_id
 }
 
-output "log_group_arns" {
-  description = "Map of log group keys to their ARNs"
-  value       = module.cloudwatch_log_group.log_group_arns
+output "secret_arn" {
+  description = "The ARN of the secret."
+  value       = module.secrets_manager_secret.secret_arn
 }
 
-output "lambda_execution_role_arn" {
-  description = "ARN of the Lambda execution IAM role"
-  value       = module.lambda_execution_role.role_arn
+output "secret_name" {
+  description = "The name of the secret."
+  value       = module.secrets_manager_secret.secret_name
 }
 
-output "lambda_execution_role_name" {
-  description = "Name of the Lambda execution IAM role"
-  value       = module.lambda_execution_role.role_name
+output "secret_version_id" {
+  description = "The unique identifier of the version of the secret."
+  value       = module.secrets_manager_secret.secret_version_id
 }
 
-output "lambda_execution_role_id" {
-  description = "Stable ID of the Lambda execution IAM role"
-  value       = module.lambda_execution_role.role_id
+output "rotation_enabled" {
+  description = "Whether automatic rotation is enabled for the secret."
+  value       = module.secrets_manager_secret.rotation_enabled
 }
 
-output "lambda_security_group_id" {
-  description = "ID of the Lambda security group"
-  value       = module.lambda_security_group.security_group_id
-}
-
-output "lambda_security_group_arn" {
-  description = "ARN of the Lambda security group"
-  value       = module.lambda_security_group.security_group_arn
-}
-
-output "lambda_security_group_name" {
-  description = "Name of the Lambda security group"
-  value       = module.lambda_security_group.security_group_name
-}
-
-output "s3_cross_account_policy_arn" {
-  description = "ARN of the cross-account S3 IAM policy attached to the Lambda role"
-  value       = aws_iam_policy.cross_account_s3_test_crossaccount_opsera_demo_access.arn
-}
-
-output "secrets_manager_policy_arn" {
-  description = "ARN of the Secrets Manager IAM policy attached to the Lambda role"
-  value       = aws_iam_policy.cross_account_secretsmanager_affinity_test_secrets_access.arn
+output "kms_key_id" {
+  description = "The KMS key ID used to encrypt the secret, if any."
+  value       = module.secrets_manager_secret.kms_key_id
 }
