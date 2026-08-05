dd984892c552b3baf22566b28bac23d504791d27
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 00:16:58 +0530

    Update modules/aws-secrets-manager-secret/outputs.tf via CloudForge (auto-generated module)

--- modules/aws-secrets-manager-secret/outputs.tf
+++ modules/aws-secrets-manager-secret/outputs.tf
@@ -1,29 +1,32 @@
 output "secret_id" {
-  description = "The ID of the Secrets Manager secret."
+  description = "The ID of the secret."
   value       = aws_secretsmanager_secret.this.id
 }
 
 output "secret_arn" {
-  description = "The ARN of the Secrets Manager secret."
+  description = "The ARN of the secret."
   value       = aws_secretsmanager_secret.this.arn
 }
 
 output "secret_name" {
-  description = "The name of the Secrets Manager secret."
+  description = "The name of the secret."
   value       = aws_secretsmanager_secret.this.name
 }
 
 output "secret_version_id" {
   description = "The unique identifier of the version of the secret."
-  value       = try(aws_secretsmanager_secret_version.this[0].version_id, null)
+  value       = length(aws_secretsmanager_secret_version.this) > 0 ? aws_secretsmanager_secret_version.this[0].version_id : null
 }
 
 output "rotation_enabled" {
   description = "Whether automatic rotation is enabled for the secret."
-  value       = length(aws_secretsmanager_secret_rotation.this) > 0
+  value       = aws_secretsmanager_secret.this.rotation_enabled
 }
 
 output "replica_arns" {
-  description = "A list of ARNs of the replica secrets."
-  value       = [for r in aws_secretsmanager_secret.this.replica : r.arn]
+  description = "A map of replica region to replica secret ARN."
+  value = {
+    for r in aws_secretsmanager_secret.this.replica :
+    r.region => r.arn
+  }
 }
