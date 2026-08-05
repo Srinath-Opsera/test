9fe9cce5d479d1f1eda4bb676f16a96c0bcdc39f
Srinath Vasudevan <srinath@opsera.io>
2026-08-05 14:10:38 +0530

    Update backend.tf via CloudForge

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     organization = "TF01"
     workspaces {
       # Replaced automatically during deployment with the selected TFC workspace name
-      name = "{taggroup}-dev"
+      name = "{taggrupo}-dev"
     }
   }
 }
