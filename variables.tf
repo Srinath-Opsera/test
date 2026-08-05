77cf928e5998acf85e79890793c8267499b36adf
Srinath Vasudevan <srinath@opsera.io>
2026-08-05 16:15:49 +0530

    Update variables.tf via CloudForge

--- variables.tf
+++ variables.tf
@@ -4,18 +4,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "environment" {
-  type        = string
-  description = "Deployment environment"
-  default     = "dev"
-}
-
-variable "team" {
-  type        = string
-  description = "Team name used as 'grupo' tag"
-  default     = "platform"
-}
-
 variable "default_tags" {
   type        = map(string)
   description = "Tags applied to all AWS resources via provider default_tags"
@@ -27,6 +15,12 @@ variable "iam_policy_name" {
   description = "Name of the IAM policy for Secrets Manager access"
 }
 
+variable "iam_policy_tags" {
+  type        = map(string)
+  description = "Tags for the IAM policy"
+  default     = {}
+}
+
 variable "secret_name" {
   type        = string
   description = "The name of the Secrets Manager secret"
@@ -35,18 +29,18 @@ variable "secret_name" {
 variable "secret_description" {
   type        = string
   description = "A description of the secret"
-  default     = "Application secrets for variable-format service"
+  default     = null
 }
 
 variable "kms_key_id" {
   type        = string
-  description = "The ARN or ID of the AWS KMS key to encrypt the secret"
+  description = "The ARN or ID of the AWS KMS key to use to encrypt the secret"
   default     = null
 }
 
 variable "recovery_window_in_days" {
   type        = number
-  description = "Number of days before the secret can be deleted"
+  description = "The number of days that AWS Secrets Manager waits before it can delete the secret"
   default     = 7
 }
 
@@ -54,24 +48,25 @@ variable "force_overwrite_replica_secret" {
   type        = bool
   description = "Whether to overwrite a secret with the same name in the destination region during replication"
   default     = false
+  sensitive = true
 }
 
 variable "secret_string" {
   type        = string
-  description = "The secret value to store as a plaintext string (JSON-encoded for structured secrets)"
+  description = "The secret value to store as a plaintext string"
   sensitive   = true
   default     = null
 }
 
 variable "enable_rotation" {
   type        = bool
-  description = "Whether to enable automatic secret rotation"
+  description = "Whether to enable automatic rotation for the secret"
   default     = false
 }
 
 variable "rotation_automatically_after_days" {
   type        = number
-  description = "Number of days between automatic scheduled rotations"
+  description = "The number of days between automatic scheduled rotations of the secret"
   default     = 30
 }
 
@@ -79,4 +74,10 @@ variable "block_public_policy" {
   type        = bool
   description = "Whether to block resource-based policies that allow broad access to the secret"
   default     = true
+}
+
+variable "secret_tags" {
+  type        = map(string)
+  description = "Tags for the Secrets Manager secret"
+  default     = {}
 }
