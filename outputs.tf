83c09d4784c37cb658b3676f899657444aea417c
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 12:37:48 +0530

    Update outputs.tf via CloudForge

--- outputs.tf
+++ outputs.tf
@@ -1,99 +1,104 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc--vpc.vpc_id
+  description = "ID of the VPC"
+  value       = module.terraform-aws-vpc--vpc.vpc_id
 }
 
 output "vpc_cidr_block" {
-  description = "VPC CIDR block"
-  value       = module.vpc--vpc.vpc_cidr_block
+  description = "CIDR block of the VPC"
+  value       = module.terraform-aws-vpc--vpc.vpc_cidr_block
 }
 
-output "private_subnet_ids" {
-  description = "Private subnet IDs from VPC module"
-  value       = module.vpc--vpc.private_subnet_ids
+output "vpc_private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = module.terraform-aws-vpc--vpc.private_subnet_ids
 }
 
-output "private_subnet_1_id" {
-  description = "Private subnet 1 ID"
-  value       = module.subnet--private-subnet.subnet_id
+output "vpc_public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = module.terraform-aws-vpc--vpc.public_subnet_ids
 }
 
-output "private_subnet_2_id" {
-  description = "Private subnet 2 ID"
-  value       = module.subnet--db-subnet-group.subnet_id
+output "subnet_id" {
+  description = "ID of the subnet"
+  value       = module.terraform-aws-subnet--subnet.subnet_id
 }
 
-output "db_subnet_group_name" {
-  description = "DB subnet group name"
-  value       = aws_db_subnet_group.opsera_test.name
+output "subnet_cidr_block" {
+  description = "CIDR block of the subnet"
+  value       = module.terraform-aws-subnet--subnet.subnet_cidr_block
 }
 
-output "lambda_sg_id" {
-  description = "Lambda security group ID"
-  value       = module.security-group--lambda-security-group.security_group_id
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = module.terraform-aws-security-group--lambda-security-group.security_group_id
 }
 
-output "rds_sg_id" {
-  description = "RDS security group ID"
-  value       = module.security-group--aurora-postgresql-rds-security-group.security_group_id
+output "security_group_arn" {
+  description = "ARN of the security group"
+  value       = module.terraform-aws-security-group--lambda-security-group.security_group_arn
 }
 
-output "s3_bucket_id" {
-  description = "S3 bucket name"
-  value       = module.s3--s3-bucket.bucket_id
+output "iam_role_arn" {
+  description = "ARN of the IAM role"
+  value       = module.terraform-aws-iam-role--lambda-execution-iam-role.role_arn
 }
 
-output "s3_bucket_arn" {
-  description = "S3 bucket ARN"
-  value       = module.s3--s3-bucket.bucket_arn
+output "iam_role_name" {
+  description = "Name of the IAM role"
+  value       = module.terraform-aws-iam-role--lambda-execution-iam-role.role_name
 }
 
-output "lambda_log_group_names" {
-  description = "CloudWatch log group names"
-  value       = module.cloudwatch--cloudwatch-log-group-lambda.log_group_names
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = module.aws-ecr-repository.repository_url
 }
 
-output "lambda_log_group_arns" {
-  description = "CloudWatch log group ARNs"
-  value       = module.cloudwatch--cloudwatch-log-group-lambda.log_group_arns
+output "ecr_repository_arn" {
+  description = "ARN of the ECR repository"
+  value       = module.aws-ecr-repository.repository_arn
 }
 
-output "secret_arn" {
-  description = "Secrets Manager secret ARN"
-  value       = module.aws-secrets-manager-secret.secret_arn
+output "cloudwatch_log_group_names" {
+  description = "Map of log group keys to their names"
+  value       = module.aws-cloudwatch--cloudwatch-log-group.log_group_names
 }
 
-output "secret_name" {
-  description = "Secrets Manager secret name"
-  value       = module.aws-secrets-manager-secret.secret_name
-}
-
-output "lambda_function_arn" {
-  description = "Lambda function ARN"
-  value       = module.lambda--lambda-function.function_arn
+output "cloudwatch_log_group_arns" {
+  description = "Map of log group keys to their ARNs"
+  value       = module.aws-cloudwatch--cloudwatch-log-group.log_group_arns
 }
 
 output "lambda_function_name" {
-  description = "Lambda function name"
-  value       = module.lambda--lambda-function.function_name
+  description = "Name of the Lambda function"
+  value       = module.aws-lambda-function.function_name
 }
 
-output "lambda_role_arn" {
-  description = "Lambda execution role ARN"
-  value       = module.lambda--lambda-function.role_arn
+output "lambda_function_arn" {
+  description = "ARN of the Lambda function"
+  value       = module.aws-lambda-function.function_arn
 }
 
-output "rds_instance_id" {
-  description = "RDS instance ID"
-  value       = module.rds--aurora-postgresql-rds.db_instance_id
+output "lambda_function_invoke_arn" {
+  description = "Invoke ARN of the Lambda function"
+  value       = module.aws-lambda-function.function_invoke_arn
 }
 
-output "rds_instance_endpoint" {
-  description = "RDS instance endpoint"
-  value       = module.rds--aurora-postgresql-rds.db_instance_endpoint
+output "lambda_iam_role_arn" {
+  description = "ARN of the Lambda IAM execution role"
+  value       = module.aws-lambda-function.iam_role_arn
 }
 
-output "rds_instance_arn" {
-  description = "RDS instance ARN"
-  value       = module.rds--aurora-postgresql-rds.db_instance_arn
+output "lambda_iam_role_name" {
+  description = "Name of the Lambda IAM execution role"
+  value       = module.aws-lambda-function.iam_role_name
+}
+
+output "lambda_cloudwatch_log_group_name" {
+  description = "Name of the Lambda CloudWatch log group"
+  value       = module.aws-lambda-function.cloudwatch_log_group_name
+}
+
+output "cross_account_s3_policy_arn" {
+  description = "ARN of the cross-account S3 access policy"
+  value       = aws_iam_policy.cross_account_s3_test_crossaccount_opsera_demo_access.arn
 }
