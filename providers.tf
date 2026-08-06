873495af18dbbb943ede0aefe73997da8fce6140
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 19:57:34 +0530

    Update providers.tf via CloudForge

--- providers.tf
+++ providers.tf
@@ -14,17 +14,3 @@ provider "aws" {
     tags = var.default_tags
   }
 }
-
-provider "aws" {
-  alias  = "acct_792373136340"
-  region = var.region
-
-  assume_role {
-    role_arn    = var.assume_role_arn_acct_792373136340
-    external_id = var.assume_role_external_id_acct_792373136340 != "" ? var.assume_role_external_id_acct_792373136340 : null
-  }
-
-  default_tags {
-    tags = var.default_tags
-  }
-}
