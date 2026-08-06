f8248a31b786991c1ef0fc1fb66b466e76480776
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 12:37:33 +0530

    Update modules/aws-lambda-function/outputs.tf via CloudForge (auto-generated module)

--- modules/aws-lambda-function/outputs.tf
+++ modules/aws-lambda-function/outputs.tf
@@ -28,39 +28,39 @@ output "function_last_modified" {
   value       = aws_lambda_function.this.last_modified
 }
 
-output "role_arn" {
-  description = "The ARN of the IAM execution role attached to the Lambda function."
-  value       = var.create_iam_role ? aws_iam_role.this[0].arn : var.existing_role_arn
+output "iam_role_arn" {
+  description = "The ARN of the IAM execution role for the Lambda function."
+  value       = var.create_iam_role ? aws_iam_role.this[0].arn : var.existing_iam_role_arn
 }
 
-output "role_name" {
-  description = "The name of the IAM execution role attached to the Lambda function."
+output "iam_role_name" {
+  description = "The name of the IAM execution role for the Lambda function."
   value       = var.create_iam_role ? aws_iam_role.this[0].name : null
 }
 
-output "log_group_name" {
+output "cloudwatch_log_group_name" {
   description = "The name of the CloudWatch log group for the Lambda function."
-  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].name : "/aws/lambda/${var.function_name}"
+  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].name : "/aws/lambda/${aws_lambda_function.this.function_name}"
 }
 
-output "log_group_arn" {
+output "cloudwatch_log_group_arn" {
   description = "The ARN of the CloudWatch log group for the Lambda function."
   value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].arn : null
 }
 
 output "security_group_id" {
-  description = "The ID of the security group created for the Lambda function (VPC deployments only)."
+  description = "The ID of the security group created for the Lambda function."
   value       = var.create_security_group && var.vpc_subnet_ids != null ? aws_security_group.this[0].id : null
 }
 
-output "alias_arn" {
-  description = "The ARN of the Lambda alias."
-  value       = var.create_alias ? aws_lambda_alias.this[0].arn : null
+output "alias_arns" {
+  description = "Map of Lambda alias names to their ARNs."
+  value       = { for k, v in aws_lambda_alias.this : k => v.arn }
 }
 
-output "alias_invoke_arn" {
-  description = "The ARN to be used for invoking the Lambda alias from API Gateway."
-  value       = var.create_alias ? aws_lambda_alias.this[0].invoke_arn : null
+output "alias_invoke_arns" {
+  description = "Map of Lambda alias names to their invoke ARNs."
+  value       = { for k, v in aws_lambda_alias.this : k => v.invoke_arn }
 }
 
 output "function_url" {
@@ -71,4 +71,9 @@ output "function_url" {
 output "function_url_id" {
   description = "The unique identifier for the Lambda function URL."
   value       = var.create_function_url ? aws_lambda_function_url.this[0].url_id : null
+}
+
+output "event_source_mapping_ids" {
+  description = "Map of event source mapping keys to their UUIDs."
+  value       = { for k, v in aws_lambda_event_source_mapping.this : k => v.uuid }
 }
