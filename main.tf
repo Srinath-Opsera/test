c4aff053fb069671bed7e16c200b0246617b749a
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 11:22:01 +0530

    Update main.tf via CloudForge

--- main.tf
+++ main.tf
@@ -1,211 +1,221 @@
 # VPC
-module "terraform-aws-vpc--virtual-private-cloud-vpc" {
-  source = "./modules/terraform-aws-vpc"
+module "vpc--vpc" {
+  source  = "app.terraform.io/TF01/vpc/aws"
+  version = "~> 1.0.0"
 
   name                    = var.vpc_name
   cidr_block              = var.vpc_cidr_block
-  availability_zones      = var.vpc_availability_zones
-  public_subnet_cidrs     = var.vpc_public_subnet_cidrs
-  private_subnet_cidrs    = var.vpc_private_subnet_cidrs
-  enable_nat_gateway      = var.vpc_enable_nat_gateway
-  single_nat_gateway      = var.vpc_single_nat_gateway
-  enable_dns_hostnames    = var.vpc_enable_dns_hostnames
-  enable_dns_support      = var.vpc_enable_dns_support
-  map_public_ip_on_launch = var.vpc_map_public_ip_on_launch
+  availability_zones      = var.availability_zones
+  public_subnet_cidrs     = var.public_subnet_cidrs
+  private_subnet_cidrs    = var.private_subnet_cidrs
+  enable_dns_support      = var.enable_dns_support
+  enable_dns_hostnames    = var.enable_dns_hostnames
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+  enable_nat_gateway      = var.enable_nat_gateway
+  single_nat_gateway      = var.single_nat_gateway
   tags                    = {}
 }
 
-# Internet Gateway
-module "aws-internet-gateway" {
-  source = "./modules/aws-internet-gateway"
+# Lambda Security Group
+module "security-group--lambda-security-group" {
+  source  = "app.terraform.io/TF01/security-group/aws"
+  version = "~> 1.0.0"
 
-  name   = var.igw_name
-  vpc_id = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  tags   = {}
-}
-
-# NAT Gateway
-module "aws-nat-gateway" {
-  source = "./modules/aws-nat-gateway"
-
-  name              = var.nat_name
-  subnet_ids        = [module.terraform-aws-subnet--public-subnet.subnet_id]
-  nat_gateway_count = var.nat_gateway_count
-  connectivity_type = var.nat_connectivity_type
-  create_eip        = var.nat_create_eip
-  tags              = {}
-}
-
-# Public Subnet
-module "terraform-aws-subnet--public-subnet" {
-  source = "./modules/terraform-aws-subnet"
-
-  name                            = var.public_subnet_name
-  vpc_id                          = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  cidr_block                      = var.public_subnet_cidr_block
-  availability_zone               = var.public_subnet_availability_zone
-  map_public_ip_on_launch         = var.public_subnet_map_public_ip_on_launch
-  assign_ipv6_address_on_creation = var.public_subnet_assign_ipv6_address_on_creation
-  create_route_table              = var.public_subnet_create_route_table
-  default_route_target_id         = module.aws-internet-gateway.id
-  default_route_target_type       = var.public_subnet_default_route_target_type
-  tags                            = {}
-}
-
-# Private Subnet
-module "terraform-aws-subnet--private-subnet" {
-  source = "./modules/terraform-aws-subnet"
-
-  name                            = var.private_subnet_name
-  vpc_id                          = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  cidr_block                      = var.private_subnet_cidr_block
-  availability_zone               = var.private_subnet_availability_zone
-  map_public_ip_on_launch         = var.private_subnet_map_public_ip_on_launch
-  assign_ipv6_address_on_creation = var.private_subnet_assign_ipv6_address_on_creation
-  create_route_table              = var.private_subnet_create_route_table
-  default_route_target_id         = module.aws-nat-gateway.nat_gateway_ids[0]
-  default_route_target_type       = var.private_subnet_default_route_target_type
-  tags                            = {}
-}
-
-# ALB Security Group
-module "terraform-aws-security-group--alb-security-group" {
-  source = "./modules/terraform-aws-security-group"
-
-  name                     = var.alb_sg_name
-  vpc_id                   = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  description              = var.alb_sg_description
-  ingress_rules            = var.alb_sg_ingress_rules
-  egress_rules             = var.alb_sg_egress_rules
-  default_egress_allow_all = var.alb_sg_default_egress_allow_all
-  revoke_rules_on_delete   = var.alb_sg_revoke_rules_on_delete
+  name                     = var.lambda_sg_name
+  description              = var.lambda_sg_description
+  vpc_id                   = module.vpc--vpc.vpc_id
+  ingress_rules            = []
+  egress_rules             = []
+  default_egress_allow_all = true
+  revoke_rules_on_delete   = false
   tags                     = {}
 }
 
-# ECS Security Group
-module "terraform-aws-security-group--ecs-security-group" {
-  source = "./modules/terraform-aws-security-group"
+# RDS Security Group
+module "security-group--aurora-postgresql-rds-security-group" {
+  source  = "app.terraform.io/TF01/security-group/aws"
+  version = "~> 1.0.0"
 
-  name                     = var.ecs_sg_name
-  vpc_id                   = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  description              = var.ecs_sg_description
-  ingress_rules            = var.ecs_sg_ingress_rules
-  egress_rules             = var.ecs_sg_egress_rules
-  default_egress_allow_all = var.ecs_sg_default_egress_allow_all
-  revoke_rules_on_delete   = var.ecs_sg_revoke_rules_on_delete
+  name        = var.rds_sg_name
+  description = var.rds_sg_description
+  vpc_id      = module.vpc--vpc.vpc_id
+  ingress_rules = [
+    {
+      description      = "Allow inbound from Lambda SG"
+      from_port        = 5432
+      to_port          = 5432
+      protocol         = "tcp"
+      cidr_blocks      = []
+      ipv6_cidr_blocks = []
+      security_groups  = [module.security-group--lambda-security-group.security_group_id]
+      self             = false
+    }
+  ]
+  egress_rules             = []
+  default_egress_allow_all = true
+  revoke_rules_on_delete   = false
   tags                     = {}
 }
 
-# Application Load Balancer
-module "terraform-aws-alb--application-load-balancer" {
-  source = "./modules/terraform-aws-alb"
+# Private Subnet 1
+module "subnet--private-subnet" {
+  source  = "app.terraform.io/TF01/subnet/aws"
+  version = "~> 1.0.0"
 
-  name                        = var.alb_name
-  vpc_id                      = module.terraform-aws-vpc--virtual-private-cloud-vpc.vpc_id
-  subnet_ids                  = module.terraform-aws-vpc--virtual-private-cloud-vpc.public_subnet_ids
-  security_group_ids          = [module.terraform-aws-security-group--alb-security-group.security_group_id]
-  certificate_arn             = var.alb_certificate_arn
-  internal                    = var.alb_internal
-  enable_deletion_protection  = var.alb_enable_deletion_protection
-  idle_timeout                = var.alb_idle_timeout
-  target_port                 = var.alb_target_port
-  target_protocol             = var.alb_target_protocol
-  health_check_path           = var.alb_health_check_path
-  ssl_policy                  = var.alb_ssl_policy
-  additional_certificate_arns = var.alb_additional_certificate_arns
-  tags                        = {}
+  name                            = "${var.private_subnet_name}-1"
+  vpc_id                          = module.vpc--vpc.vpc_id
+  cidr_block                      = var.private_subnet_cidr_1
+  availability_zone               = var.private_subnet_az_1
+  map_public_ip_on_launch         = false
+  assign_ipv6_address_on_creation = false
+  ipv6_cidr_block                 = null
+  additional_routes               = []
+  default_route_target_type       = "nat_gateway_id"
+  default_route_target_id         = length(module.vpc--vpc.nat_gateway_ids) > 0 ? module.vpc--vpc.nat_gateway_ids[0] : null
+  create_route_table              = true
+  tags                            = {}
 }
 
-# CloudWatch Log Group
-module "aws-cloudwatch--cloudwatch-log-group" {
-  source = "./modules/aws-cloudwatch"
+# Private Subnet 2 (for DB subnet group multi-AZ)
+module "subnet--db-subnet-group" {
+  source  = "app.terraform.io/TF01/subnet/aws"
+  version = "~> 1.0.0"
 
-  log_groups    = var.log_groups
-  metric_alarms = var.metric_alarms
-  dashboards    = var.dashboards
-  log_streams   = var.log_streams
-  event_rules   = var.event_rules
-  event_targets = var.event_targets
+  name                            = "${var.private_subnet_name}-2"
+  vpc_id                          = module.vpc--vpc.vpc_id
+  cidr_block                      = var.private_subnet_cidr_2
+  availability_zone               = var.private_subnet_az_2
+  map_public_ip_on_launch         = false
+  assign_ipv6_address_on_creation = false
+  ipv6_cidr_block                 = null
+  additional_routes               = []
+  default_route_target_type       = "nat_gateway_id"
+  default_route_target_id         = length(module.vpc--vpc.nat_gateway_ids) > 0 ? module.vpc--vpc.nat_gateway_ids[0] : null
+  create_route_table              = true
+  tags                            = {}
+}
+
+# DB Subnet Group
+resource "aws_db_subnet_group" "opsera_test" {
+  name = var.db_subnet_group_name
+  subnet_ids = [
+    module.subnet--private-subnet.subnet_id,
+    module.subnet--db-subnet-group.subnet_id
+  ]
+
+  tags = {
+    Name = var.db_subnet_group_name
+  }
+}
+
+# S3 Bucket
+module "s3--s3-bucket" {
+  source  = "app.terraform.io/TF01/s3/aws"
+  version = "~> 1.0.0"
+
+  bucket_name             = var.bucket_name
+  versioning_enabled      = var.versioning_enabled
+  sse_algorithm           = var.sse_algorithm
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+  force_destroy           = var.force_destroy
+  lifecycle_rules         = []
+  bucket_policy_json      = null
+  kms_master_key_id       = null
+  tags                    = {}
+}
+
+# CloudWatch Log Group for Lambda
+module "cloudwatch--cloudwatch-log-group-lambda" {
+  source  = "app.terraform.io/TF01/cloudwatch/aws"
+  version = "~> 1.0.1"
+
+  log_groups = {
+    lambda_log_group = {
+      name              = var.lambda_log_group_name
+      retention_in_days = var.lambda_log_retention_days
+      kms_key_id        = null
+      tags              = {}
+    }
+  }
+  event_rules   = {}
+  event_targets = {}
+  log_streams   = {}
+  dashboards    = {}
+  metric_alarms = {}
   tags          = {}
 }
 
-# ECR Repository
-module "aws-ecr-repository" {
-  source = "./modules/aws-ecr-repository"
+# Secrets Manager Secret
+module "aws-secrets-manager-secret" {
+  source = "./modules/aws-secrets-manager-secret"
 
-  name                 = var.ecr_name
-  image_tag_mutability = var.ecr_image_tag_mutability
-  scan_on_push         = var.ecr_scan_on_push
-  encryption_type      = var.ecr_encryption_type
-  force_delete         = var.ecr_force_delete
-  tags                 = {}
+  name                              = var.secret_name
+  description                       = var.secret_description
+  kms_key_id                        = null
+  recovery_window_in_days           = var.recovery_window_in_days
+  force_overwrite_replica_secret    = false
+  replica_regions                   = []
+  secret_string                     = var.secret_string
+  secret_is_json                    = false
+  secret_string_json                = {}
+  secret_binary                     = null
+  version_stages                    = null
+  enable_rotation                   = false
+  rotation_lambda_arn               = null
+  rotation_automatically_after_days = 30
+  secret_policy                     = null
+  block_public_policy               = true
+  tags                              = {}
 }
 
-# ECS Task Execution IAM Role
-module "terraform-aws-iam-role--ecs-task-execution-iam-role" {
-  source = "./modules/terraform-aws-iam-role"
+# Lambda Function
+module "lambda--lambda-function" {
+  source  = "app.terraform.io/TF01/lambda/aws"
+  version = "~> 1.0.0"
 
-  name                   = var.ecs_exec_role_name
-  assume_role_principals = var.ecs_exec_role_assume_role_principals
-  description            = var.ecs_exec_role_description
-  path                   = var.ecs_exec_role_path
-  max_session_duration   = var.ecs_exec_role_max_session_duration
-  managed_policy_arns    = var.ecs_exec_role_managed_policy_arns
-  inline_policies        = var.ecs_exec_role_inline_policies
-  force_detach_policies  = var.ecs_exec_role_force_detach_policies
-  tags                   = {}
+  function_name                  = var.lambda_function_name
+  runtime                        = var.lambda_runtime
+  handler                        = var.lambda_handler
+  package_type                   = var.lambda_package_type
+  filename                       = var.lambda_filename
+  description                    = ""
+  memory_size                    = var.lambda_memory_size
+  timeout                        = var.lambda_timeout
+  reserved_concurrent_executions = var.lambda_reserved_concurrent_executions
+  architectures                  = var.lambda_architectures
+  environment_variables          = var.lambda_environment_variables
+  log_retention_days             = var.lambda_log_retention_days
+  additional_policy_arns         = var.lambda_additional_policy_arns
+  image_uri                      = null
+  source_code_hash               = null
+  tags                           = {}
 }
 
-# ECS Task IAM Role
-module "terraform-aws-iam-role--ecs-task-iam-role" {
-  source = "./modules/terraform-aws-iam-role"
+# RDS Aurora PostgreSQL
+module "rds--aurora-postgresql-rds" {
+  source  = "app.terraform.io/TF01/rds/aws"
+  version = "~> 1.0.0"
 
-  name                   = var.ecs_task_role_name
-  assume_role_principals = var.ecs_task_role_assume_role_principals
-  description            = var.ecs_task_role_description
-  path                   = var.ecs_task_role_path
-  max_session_duration   = var.ecs_task_role_max_session_duration
-  managed_policy_arns    = var.ecs_task_role_managed_policy_arns
-  inline_policies        = var.ecs_task_role_inline_policies
-  force_detach_policies  = var.ecs_task_role_force_detach_policies
-  tags                   = {}
-}
-
-# ECS Fargate (Cluster + Service + Task Definition)
-module "terraform-aws-ecs-fargate--ecs-cluster" {
-  source = "./modules/terraform-aws-ecs-fargate"
-
-  cluster_name           = var.ecs_cluster_name
-  service_name           = var.ecs_cluster_service_name
-  task_family            = var.ecs_cluster_task_family
-  container_name         = var.ecs_cluster_container_name
-  container_image        = var.container_image
-  subnet_ids             = module.terraform-aws-vpc--virtual-private-cloud-vpc.private_subnet_ids
-  security_group_ids     = [module.terraform-aws-security-group--ecs-security-group.security_group_id]
-  execution_role_arn     = module.terraform-aws-iam-role--ecs-task-execution-iam-role.role_arn
-  task_role_arn          = module.terraform-aws-iam-role--ecs-task-iam-role.role_arn
-  cpu                    = var.ecs_cluster_cpu
-  memory                 = var.ecs_cluster_memory
-  container_port         = var.ecs_cluster_container_port
-  desired_count          = var.ecs_cluster_desired_count
-  assign_public_ip       = var.ecs_cluster_assign_public_ip
-  target_group_arn       = module.terraform-aws-alb--application-load-balancer.target_group_arn
-  enable_autoscaling     = var.ecs_cluster_enable_autoscaling
-  autoscaling_min        = var.ecs_cluster_autoscaling_min
-  autoscaling_max        = var.ecs_cluster_autoscaling_max
-  autoscaling_cpu_target = var.ecs_cluster_autoscaling_cpu_target
-  log_retention_days     = var.ecs_cluster_log_retention_days
-  tags                   = {}
-}
-
-# Auto Scaling Policy
-module "aws-auto-scaling-policy" {
-  source = "./modules/aws-auto-scaling-policy"
-
-  name                          = var.asg_policy_name
-  autoscaling_group_name        = var.asg_policy_autoscaling_group_name
-  policy_type                   = var.asg_policy_type
-  cooldown                      = var.asg_policy_cooldown
-  target_tracking_configuration = var.asg_policy_target_tracking_configuration
-  tags                          = {}
+  identifier              = var.rds_identifier
+  engine                  = var.rds_engine
+  engine_version          = var.rds_engine_version
+  username                = var.rds_username
+  password                = var.rds_password
+  db_name                 = var.rds_db_name
+  db_subnet_group_name    = aws_db_subnet_group.opsera_test.name
+  vpc_security_group_ids  = [module.security-group--aurora-postgresql-rds-security-group.security_group_id]
+  instance_class          = var.rds_instance_class
+  allocated_storage       = var.rds_allocated_storage
+  max_allocated_storage   = var.rds_max_allocated_storage
+  storage_type            = var.rds_storage_type
+  storage_encrypted       = var.rds_storage_encrypted
+  kms_key_id              = null
+  multi_az                = var.rds_multi_az
+  backup_retention_period = var.rds_backup_retention_period
+  skip_final_snapshot     = var.rds_skip_final_snapshot
+  deletion_protection     = var.rds_deletion_protection
+  tags                    = {}
 }
