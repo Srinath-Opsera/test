57047868cbda03ef3dc5a017e6e8241893e6ca21
Srinath Vasudevan <srinath@opsera.io>
2026-08-06 12:37:30 +0530

    Update modules/aws-ecr-repository/outputs.tf via CloudForge (auto-generated module)

--- modules/aws-ecr-repository/outputs.tf
+++ modules/aws-ecr-repository/outputs.tf
@@ -9,21 +9,21 @@ output "repository_arn" {
 }
 
 output "repository_url" {
-  description = "The URL of the ECR repository (used for docker push/pull)."
+  description = "The URL of the ECR repository, in the form of aws_account_id.dkr.ecr.region.amazonaws.com/repository_name."
   value       = aws_ecr_repository.this.repository_url
 }
 
 output "registry_id" {
-  description = "The registry ID (AWS account ID) where the repository was created."
+  description = "The registry ID where the repository was created."
   value       = aws_ecr_repository.this.registry_id
 }
 
 output "lifecycle_policy_id" {
-  description = "The repository name that the lifecycle policy is associated with."
+  description = "The repository name that the lifecycle policy is applied to."
   value       = length(aws_ecr_lifecycle_policy.this) > 0 ? aws_ecr_lifecycle_policy.this[0].id : null
 }
 
 output "repository_policy_id" {
-  description = "The repository name that the repository policy is associated with."
+  description = "The repository name that the repository policy is applied to."
   value       = length(aws_ecr_repository_policy.this) > 0 ? aws_ecr_repository_policy.this[0].id : null
 }
