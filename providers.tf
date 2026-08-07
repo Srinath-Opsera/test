37c7d2c3990d89cd77a8cbdb1679faf1ac43d3b5
Srinath Vasudevan <srinath@opsera.io>
2026-08-08 02:18:25 +0530

    Update providers.tf via CloudForge

--- providers.tf
+++ providers.tf
@@ -11,34 +11,13 @@ provider "aws" {
   region = var.region
 
   default_tags {
-    tags = merge(var.default_tags, {
+    tags = {
       Service     = var.service_name
       Grupo       = var.team
       Environment = upper(var.environment)
       ManagedBy   = "Opsera"
       NewRelic    = "true"
       Terraform   = "true"
-    })
-  }
-}
-
-provider "aws" {
-  alias  = "acct_792373136340"
-  region = var.region
-
-  assume_role {
-    role_arn    = var.assume_role_arn_acct_792373136340
-    external_id = var.assume_role_external_id_acct_792373136340 != "" ? var.assume_role_external_id_acct_792373136340 : null
-  }
-
-  default_tags {
-    tags = merge(var.default_tags, {
-      Service     = var.service_name
-      Grupo       = var.team
-      Environment = upper(var.environment)
-      ManagedBy   = "Opsera"
-      NewRelic    = "true"
-      Terraform   = "true"
-    })
+    }
   }
 }
