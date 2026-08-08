ddcb65d1963002fa69b2effb7c380eb3e1aaf4fb
Srinath Vasudevan <srinath@opsera.io>
2026-08-08 22:51:43 +0530

    Update modules/aws-ecr-repository/outputs.tf via CloudForge (auto-generated module)

--- modules/aws-ecr-repository/outputs.tf
+++ modules/aws-ecr-repository/outputs.tf
@@ -9,11 +9,11 @@ output "repository_arn" {
 }
 
 output "repository_url" {
-  description = "The URL of the ECR repository (used for docker push/pull)."
+  description = "The URL of the ECR repository, in the form of aws_account_id.dkr.ecr.region.amazonaws.com/repository_name."
   value       = aws_ecr_repository.this.repository_url
 }
 
-output "repository_id" {
+output "registry_id" {
   description = "The registry ID where the repository was created."
   value       = aws_ecr_repository.this.registry_id
 }
