fc9a6107ac72f74ba143245d1b2e5e658551f96e
Srinath Vasudevan <srinath@opsera.io>
2026-08-11 20:41:11 +0530

    Update modules/aws-lambda-function/outputs.tf via CloudForge (auto-generated module)

--- modules/aws-lambda-function/outputs.tf
+++ modules/aws-lambda-function/outputs.tf
@@ -1,11 +1,11 @@
-output "function_arn" {
-  description = "The ARN of the Lambda function."
-  value       = aws_lambda_function.this.arn
-}
-
 output "function_name" {
   description = "The name of the Lambda function."
   value       = aws_lambda_function.this.function_name
+}
+
+output "function_arn" {
+  description = "The ARN of the Lambda function."
+  value       = aws_lambda_function.this.arn
 }
 
 output "function_qualified_arn" {
@@ -28,47 +28,47 @@ output "function_last_modified" {
   value       = aws_lambda_function.this.last_modified
 }
 
+output "role_arn" {
+  description = "The ARN of the IAM execution role attached to the Lambda function."
+  value       = var.create_iam_role ? aws_iam_role.this[0].arn : var.existing_role_arn
+}
+
+output "role_name" {
+  description = "The name of the IAM execution role attached to the Lambda function."
+  value       = var.create_iam_role ? aws_iam_role.this[0].name : null
+}
+
+output "cloudwatch_log_group_name" {
+  description = "The name of the CloudWatch log group for the Lambda function."
+  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].name : "/aws/lambda/${aws_lambda_function.this.function_name}"
+}
+
+output "cloudwatch_log_group_arn" {
+  description = "The ARN of the CloudWatch log group for the Lambda function."
+  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].arn : null
+}
+
 output "alias_arn" {
-  description = "The ARN of the Lambda alias, if created."
+  description = "The ARN of the Lambda alias (if created)."
   value       = var.create_alias ? aws_lambda_alias.this[0].arn : null
 }
 
+output "alias_name" {
+  description = "The name of the Lambda alias (if created)."
+  value       = var.create_alias ? aws_lambda_alias.this[0].name : null
+}
+
 output "alias_invoke_arn" {
-  description = "The ARN used to invoke the Lambda alias from API Gateway, if created."
+  description = "The ARN used to invoke the Lambda alias from API Gateway (if created)."
   value       = var.create_alias ? aws_lambda_alias.this[0].invoke_arn : null
 }
 
 output "function_url" {
-  description = "The HTTPS URL of the Lambda function URL endpoint, if created."
+  description = "The HTTPS URL endpoint for the Lambda function URL (if created)."
   value       = var.create_function_url ? aws_lambda_function_url.this[0].function_url : null
 }
 
 output "function_url_id" {
-  description = "The unique identifier for the Lambda function URL, if created."
+  description = "The unique ID of the Lambda function URL (if created)."
   value       = var.create_function_url ? aws_lambda_function_url.this[0].url_id : null
-}
-
-output "iam_role_arn" {
-  description = "The ARN of the IAM execution role, if created by this module."
-  value       = var.create_iam_role ? aws_iam_role.this[0].arn : null
-}
-
-output "iam_role_name" {
-  description = "The name of the IAM execution role, if created by this module."
-  value       = var.create_iam_role ? aws_iam_role.this[0].name : null
-}
-
-output "security_group_id" {
-  description = "The ID of the security group created for the Lambda function, if applicable."
-  value       = var.create_security_group && var.vpc_subnet_ids != null ? aws_security_group.this[0].id : null
-}
-
-output "cloudwatch_log_group_name" {
-  description = "The name of the CloudWatch log group for the Lambda function, if created."
-  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].name : null
-}
-
-output "cloudwatch_log_group_arn" {
-  description = "The ARN of the CloudWatch log group for the Lambda function, if created."
-  value       = var.create_cloudwatch_log_group ? aws_cloudwatch_log_group.this[0].arn : null
 }
