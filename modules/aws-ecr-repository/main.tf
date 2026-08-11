07a02e9098d32890921d37f2ac69fca30d772230
Srinath Vasudevan <srinath@opsera.io>
2026-08-11 20:41:02 +0530

    Update modules/aws-ecr-repository/main.tf via CloudForge (auto-generated module)

--- modules/aws-ecr-repository/main.tf
+++ modules/aws-ecr-repository/main.tf
@@ -24,22 +24,19 @@ resource "aws_ecr_repository" "this" {
 
   force_delete = var.force_delete
 
-  tags = merge(
-    var.tags,
-    {
-      Name = var.name
-    }
-  )
+  tags = var.tags
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
-  count      = var.lifecycle_policy != null ? 1 : 0
+  count = var.lifecycle_policy != null ? 1 : 0
+
   repository = aws_ecr_repository.this.name
   policy     = var.lifecycle_policy
 }
 
 resource "aws_ecr_repository_policy" "this" {
-  count      = var.repository_policy != null ? 1 : 0
+  count = var.repository_policy != null ? 1 : 0
+
   repository = aws_ecr_repository.this.name
   policy     = var.repository_policy
 }
