baed803734564316f500a23efec8261e316c39f9
Srinath Vasudevan <srinath@opsera.io>
2026-08-11 20:41:21 +0530

    Add modules/aws-secrets-manager/outputs.tf via CloudForge (auto-generated module)

---
+++ modules/aws-secrets-manager/outputs.tf
@@ -0,0 +1,32 @@
+output "secret_id" {
+  description = "The ID of the secret."
+  value       = aws_secretsmanager_secret.this.id
+}
+
+output "secret_arn" {
+  description = "The ARN of the secret."
+  value       = aws_secretsmanager_secret.this.arn
+}
+
+output "secret_name" {
+  description = "The name of the secret."
+  value       = aws_secretsmanager_secret.this.name
+}
+
+output "secret_version_id" {
+  description = "The unique identifier of the version of the secret."
+  value       = length(aws_secretsmanager_secret_version.this) > 0 ? aws_secretsmanager_secret_version.this[0].version_id : null
+}
+
+output "rotation_enabled" {
+  description = "Whether automatic rotation is enabled for the secret."
+  value       = length(aws_secretsmanager_secret_rotation.this) > 0
+}
+
+output "replica_arns" {
+  description = "A map of replica region to replica secret ARN."
+  value = {
+    for r in aws_secretsmanager_secret.this.replica :
+    r.region => r.last_accessed_date
+  }
+}
