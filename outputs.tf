1b6bbfb098ae148c84c8486dc75b4eefcce5f689
Srinath Vasudevan <srinath@opsera.io>
2026-08-17 22:49:31 +0530

    Add outputs.tf via CloudForge

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "bucket_id" {
+  value = module.terraform_aws_s3.bucket_id
+}
+
+output "bucket_arn" {
+  value = module.terraform_aws_s3.bucket_arn
+}
+
+output "bucket_domain_name" {
+  value = module.terraform_aws_s3.bucket_domain_name
+}
+
+output "bucket_regional_domain_name" {
+  value = module.terraform_aws_s3.bucket_regional_domain_name
+}
